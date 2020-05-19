7ffad92b5cb9cf7a7c41664a33e53d1c5f421a51
Gouda_Clark <mclarkuga@gmail.com>
2020-05-18 21:55:45 -0400

    pulling in-memory-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,12 +24,20 @@ import { LeaderComponent } from './leader/leader.component';
     MusicianComponent,
     ActorComponent,
     BusinessComponent,
-    LeaderComponent
+    LeaderComponent,
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/vip.service.ts
+++ src/app/vip.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Vip } from './vip';
-import { VIPS } from './mock-vips';
+// import { VIPS } from './mock-vips';
 import { Observable, of } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VipService {
-
-  constructor() { }
+  private vipsUrl = 'api/vips';  // URL to web api
+  constructor(private http: HttpClient) { }
   getVips(): Observable<Vip[]> {
-    return of(VIPS);
+    return this.http.get<Vip[]>(this.vipsUrl);
   }
   getVip(id: number): Observable<Vip> {
-    return of(VIPS.find(vip => vip.id === id));
+    const url = `${this.vipsUrl}/${id}`;
+    return this.http.get<Vip>(url);
   }
 }
