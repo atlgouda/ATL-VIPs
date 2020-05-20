026f1a923bc6d7ff55be2c9fd1b42bd1b325b36c
Gouda_Clark <mclarkuga@gmail.com>
2020-05-20 11:06:24 -0400

    rm error code

--- src/app/vip.service.ts
+++ src/app/vip.service.ts
@@ -26,22 +26,6 @@ export class VipService {
   addVip(vip: Vip): Observable<Vip> {
     return this.http.post<Vip>(this.vipsUrl, vip, this.httpOptions)
   }
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-  
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-  
-      // TODO: better job of transforming error for user consumption
-      // this.log(`${operation} failed: ${error.message}`);
-  
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-}
-/**
- * Handle Http operation that failed.
- * Let the app continue.
 
- */
+}
+
